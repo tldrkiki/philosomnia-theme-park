6ae959d6894d1213936649beb9c3147ea83b8685
tldrkiki <tldr.kiki@gmail.com>
2026-09-04 12:43:19 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import { AttractionModal } from './components/AttractionModal';
